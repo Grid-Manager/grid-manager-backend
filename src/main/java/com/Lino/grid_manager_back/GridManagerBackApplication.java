4965230faab65002f84f26a7eb9894f25e368156
Eduardo Lino <eduardo_lino@estudante.sesisenai.org.br>
2026-08-13 09:10:13 -0300

    Creating the project

---
+++ src/main/java/com/Lino/grid_manager_back/GridManagerBackApplication.java
@@ -0,0 +1,13 @@
+package com.Lino.grid_manager_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridManagerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lino/grid_manager_back/GridManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lino.grid_manager_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridManagerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
