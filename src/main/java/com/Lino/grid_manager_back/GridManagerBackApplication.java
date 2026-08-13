4d7f757656f6361d17a119c4ec9e3707a89ae416
Eduardo Lino <eduardo_lino@estudante.sesisenai.org.br>
2026-08-13 09:19:44 -0300

    chore: inicializa estrutura base do projeto

--- src/main/java/com/Lino/grid_manager_back/GridManagerBackApplication.java
+++ src/main/java/com/Lino/grid_manager_back/GridManagerBackApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GridManagerBackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GridManagerBackApplication.class, args);
+		SpringApplication.run(GridManagerBackApplication.class, args); 
 	}
 
 }
