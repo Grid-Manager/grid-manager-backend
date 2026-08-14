9f49eb8fed804efc39fa672ef9f0de0ad0365c78
Eduardo Lino <eduardo_lino@estudante.sesisenai.org.br>
2026-08-14 08:45:33 -0300

    feat: start the structure

---
+++ src/main/java/com/Lino/grid_manager_back/pilot/controller/PilotController.java
@@ -0,0 +1,5 @@
+package com.Lino.grid_manager_back.pilot.controller;
+
+public class PilotController {
+
+}
---
+++ src/main/java/com/Lino/grid_manager_back/pilot/service/PilotService.java
@@ -0,0 +1,5 @@
+package com.Lino.grid_manager_back.pilot.service;
+
+public class PilotService {
+
+}
